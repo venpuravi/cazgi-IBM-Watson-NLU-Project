a37b3ca356a338b4b1609de2bcb4ddc3cf1843e2
Bala d <venpuravi@gmail.com>
2021-07-05 05:47:56 +0000

    table display map

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -5,20 +5,18 @@ class EmotionTable extends React.Component {
     render() {
       return (  
         <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions.result.emotion.document)}
+       
           <table className="table table-bordered">
             <tbody>
             {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
-
-                 this.props.emotions.result.emotion.document.emotion && 
-                  Object.keys(this.props.emotions.result.emotion.document.emotion).map(function (element,value) {
+                Object.keys(this.props.emotions.result.emotion.document.emotion).map(function (element,value) {
                      return <tr>
                        <td>{element}</td>
                        <td>{this.props.emotions.result.emotion.document.emotion[element]}</td>
                      </tr>;
-                  })
+
+                
+            },this)
             }
             </tbody>
           </table>
